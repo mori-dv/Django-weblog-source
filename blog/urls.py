6f20e853019e9a64f157c08abe5a3e45a63ab571
mori-dv <morimori.ph@gamil.com>
2021-10-29 20:29:05 +0330

    fixing

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
     path('page/<int:page>', ArticleListView.as_view(), name='home'),
     path('article/<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
-    path('category/<slug:slug>', CategoryList, name='category'),
-    path('category/<slug:slug>/page/<int:page>', CategoryList, name='category'),
+    path('category/<slug:slug>', CategoryList.as_view(), name='category'),
+    path('category/<slug:slug>/page/<int:page>', CategoryList.as_view(), name='category'),
     path('author/<slug:username>', AuthorList, name='author'),
     path('author/<slug:username>/page/<int:page>', AuthorList, name='author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -33,23 +33,24 @@ class ArticleDetailView(DetailView):
 #     return render(request, 'blog/category.html', context)
 
 class CategoryList(ListView):
-    paginated_by = 2
+    paginate_by = 2
     template_name = 'blog/category_list.html'
 
     def get_queryset(self):
         global category 
         slug = self.kwargs.get('slug')
-        category = get_object_or_404(Category.objects.active(), situation=True, slug=slug)
-        return category.articles.published()
+        category = get_object_or_404(Category.objects.active(), slug=slug)
+        return category.article.published()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = category
         return context
 
+
 class AuthorList(ListView):
-    paginated_by = 2
-    template_name = 'blog/authorList.html'
+    paginate_by = 2
+    template_name = 'blog/author_list.html'
 
     def get_queryset(self):
         global author 
