37f3dfaeda0177a65e659f653678be89a45233f4
mori-dv <morimori.ph@gamil.com>
2021-11-02 10:12:23 +0330

    some changes in html

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import ArticleListView, category, ArticleDetailView, AuthorList
+from .views import ArticleListView, CategoryList, ArticleDetailView, AuthorList
 app_name = 'blog'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
     path('page/<int:page>', ArticleListView.as_view(), name='home'),
     path('article/<slug:slug>', ArticleDetailView.as_view(), name='article_detail'),
-    path('category/<slug:slug>', category, name='category'),
-    path('category/<slug:slug>/page/<int:page>', category, name='category'),
+    path('category/<slug:slug>', CategoryList.as_view(), name='category'),
+    path('category/<slug:slug>/page/<int:page>', CategoryList.as_view(), name='category'),
     path('author/<slug:username>', AuthorList.as_view(), name='author'),
     path('author/<slug:username>/page/<int:page>', AuthorList.as_view(), name='author'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.core.paginator import Paginator
-from django.db import models
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
@@ -23,38 +22,40 @@ class ArticleDetailView(DetailView):
     
 
 # each category's page
-def category(request, slug, page=1):
-    category = get_object_or_404(Category.objects.active(), slug=slug)
-    article_list = category.article.published()
-    paginator = Paginator(article_list, 2)
-    pages = paginator.get_page(page)
-    context = {
-        'category': category,
-        'articles': pages
-    }
-    return render(request, 'blog/category_list.html', context)
+# def category(request, slug, page=1):
+#     category = get_object_or_404(Category.objects.active(), slug=slug)
+#     article_list = category.article.published()
+#     paginator = Paginator(article_list, 2)
+#     pages = paginator.get_page(page)
+#     context = {
+#         'category': category,
+#         'articles': pages
+#     }
+#     return render(request, 'blog/category_list.html', context)
 
-# pagination doesn't work and got error
-# if you can fix it, do it :)
 
-# class CategoryList(ListView):
-#     paginate_by = 2
-#     template_name = 'blog/category_list.html'
-#     def get_queryset(self):
-#         global category 
-#         slug = self.kwargs.get('slug')
-#         category = get_object_or_404(Category.objects.active(), slug=slug)
-#         return category.article.published()
+class CategoryList(ListView):
+    paginate_by = 2
+    template_name = 'blog/category_list.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        global category 
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category.objects.active(), slug=slug)
+        return category.article.published()
     
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['category'] = category
-#         return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
 
 
 class AuthorList(ListView):
     template_name = 'blog/author_list.html'
-        
+    context_object_name = 'articles'
+    paginate_by = 3
+
     def get_queryset(self):
         global author 
         username = self.kwargs.get('username')
